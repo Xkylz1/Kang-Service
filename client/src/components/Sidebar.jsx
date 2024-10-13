f55de7fcbfe1e77366b64b960dc1d906917f3199
xkylz1 <fauzansaputra741@gmail.com>
2024-10-14 03:46:35 +0800

    update: margin

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -23,7 +23,7 @@ const Sidebar = ({ onLogout }) => {
   return (
     <Nav className="d-flex flex-column bg-dark p-3 col-2">
       <div>
-        <h5 className='text-light mb-3 mt-3'>Admin Dashboard</h5>
+        <h5 className='text-light mb-3 mt-2'>Admin Dashboard</h5>
         <hr className='text-light' /> {/* Horizontal line */}
         <Nav.Link className='px-0 text-light fs-6 fw-light' href="#dashboard">Technician Assignment</Nav.Link>
         <Nav.Link className='px-0 text-light fs-6 fw-light' href="#account-management">Account Management</Nav.Link>
